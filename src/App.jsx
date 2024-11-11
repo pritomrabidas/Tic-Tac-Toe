f8623c4ab7a38a07a1f384f1eef8c8591256bc16
pritom rabidas <pritomrabidas102@gmail.com>
2024-11-11 20:41:10 +0600

    click

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,64 @@
+import { useState } from "react";
 import Box from "./Component/Box";
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const HandleClick = (index) => {
+    if (squares[index] || calculatWinner(squares)) return;
+    const newSquares = squares.slice();
+    newSquares[index] = isXNext ? "X" : "O";
+    setSquares(newSquares);
+    setIsXNext(!isXNext);
+    console.log(newSquares);
+  };
+  function calculatWinner(squares) {
+    const Lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (const [a, b, c] of Lines) {
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares;
+      }
+    }
+    return null;
+  }
+  const winner = calculatWinner(squares);
+  const status = winner
+    ? `Winner Is : ${squares}`
+    : `Next player : ${isXNext ? "X" : "O"}`;
   return (
     <section className="bg-gradient-to-l from-gray-400  to-sky-700 via-70% h-screen w-full">
       <div className="container m-auto justify-center relative top-[15%]">
         <h1 className="text-center text-3xl text-[#ffffffc7] font-nunitoFont font-semibold pb-8">
           Tic Tac Toe
         </h1>
-        <div className="rounded-lg border-2 border-white w-fit justify-center mx-auto p-10">
+        <div className="rounded-lg bg-gradient-to-r from-gray-400  to-sky-700 via-70% border-white w-fit justify-center mx-auto p-10">
           <div className="w-[500px] grid grid-cols-3 gap-11">
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
+            {squares.map((square, index) => (
+              <Box
+                key={index}
+                value={square}
+                onClick={() => HandleClick(index)}
+              />
+            ))}
           </div>
         </div>
+        <h1 className="text-center text-3xl text-[#ffffffc7] font-nunitoFont font-semibold pt-8">
+          {status}
+        </h1>
       </div>
     </section>
   );
--- src/Component/Box.jsx
+++ src/Component/Box.jsx
@@ -1,8 +1,8 @@
 
-const Box = () => {
+const Box = ({onClick,value}) => {
   return (
-    <div className="h-20 border-2 border-red-50 rounded-xl items-center justify-center flex  shadow bg-gradient-to-r from-gray-400  to-sky-700 via-70% ">
-      <h1 className="font-mono font-medium text-3xl text-[#200e0e92]">X</h1>
+    <div onClick={onClick} className="h-20 border-red-50 rounded-xl items-center justify-center flex  shadow bg-gradient-to-b from-gray-400  to-sky-700 via-10% ">
+      <h1 className="font-mono font-medium text-3xl text-[#200e0e92]">{value}</h1>
     </div>
   )
 }
